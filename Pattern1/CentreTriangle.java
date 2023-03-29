360c2b7d867fd404fbbf8e5f9d8a77d2b81f6ebd
bala07890 <balakeerathur97@gmail.com>
2023-03-29 21:44:30 +0530

    first commit

---
+++ Pattern1/CentreTriangle.java
@@ -0,0 +1,27 @@
+package Pattern1;
+
+public class CentreTriangle 
+{
+
+	public static void main(String[] args) 
+	{
+		int n=5;
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=n;j>i;j--)
+			{
+				System.out.print(" ");
+			}
+			for(int k=1;k<=i;k++)
+			{
+				System.out.print("*");
+			}
+			for(int l=1;l<i;l++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
