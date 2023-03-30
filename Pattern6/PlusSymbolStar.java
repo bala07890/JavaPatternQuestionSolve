987d36398de9ce1a254b49a40aa47520f0a2774f
bala07890 <balakeerathur97@gmail.com>
2023-03-30 08:14:51 +0530

    first commit

---
+++ Pattern6/PlusSymbolStar.java
@@ -0,0 +1,23 @@
+package Pattern6;
+
+public class PlusSymbolStar 
+{
+
+	public static void main(String[] args) 
+	{
+		int n=7;
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if(i==(n-1)/2||j==(n-1)/2)
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+
+		}
+	}
+
+}
